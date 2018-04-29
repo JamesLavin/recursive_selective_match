ddfddaf0a977267ac623fe576dd0da0fa99550e7
James Lavin <james@jameslavin.com>
2018-04-28 21:38:02 -0400

    commit new mix project

---
+++ lib/recursive_selective_match.ex
@@ -0,0 +1,18 @@
+defmodule RecursiveSelectiveMatch do
+  @moduledoc """
+  Documentation for RecursiveSelectiveMatch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RecursiveSelectiveMatch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/recursive_selective_match_test.exs
@@ -0,0 +1,8 @@
+defmodule RecursiveSelectiveMatchTest do
+  use ExUnit.Case
+  doctest RecursiveSelectiveMatch
+
+  test "greets the world" do
+    assert RecursiveSelectiveMatch.hello() == :world
+  end
+end
