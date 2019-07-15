19d662953a2141f8c630bddd402bd543c5e8d57b
James Lavin <james@jameslavin.com>
2019-07-15 13:07:57 -0400

    override to_string/1 when param is list to call inspect/1 instead of sto_string/1

--- lib/extend_string_chars.ex
+++ lib/extend_string_chars.ex
@@ -16,19 +16,22 @@ defimpl String.Chars, for: Tuple do
   end
 end
 
-# defimpl String.Chars, for: List do
-#   def to_string(list) do
-#     interior =
-#       list
-#       |> Enum.map(&inspect/1)
-#       |> Enum.join(", ")
-#
-#     "[#{interior}]"
-#   end
-# end
-
 defimpl String.Chars, for: Function do
   def to_string(fun) do
     inspect(fun)
+  end
+end
+
+# override to_string/1 when param is list to call inspect/1 instead of to_string/1
+
+defimpl String.Chars, for: List do
+  def to_string(list) do
+    inspect(list)
+    # interior =
+    #   list
+    #   |> Enum.map(&inspect/1)
+    #   |> Enum.join(", ")
+
+    # "[#{interior}]"
   end
 end
